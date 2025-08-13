8f9b4304123199710a14fbd1a4fd3e42fe7eb52d
박기순 <ks.park@sqisoft.com>
2025-08-13 14:40:19 +0900

    미디어 사이즈 가져오기 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:desktop_drop/desktop_drop.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:logger/web.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:media_kit_video/media_kit_video.dart';
+import 'package:media_kit_video/media_kit_video_controls/media_kit_video_controls.dart'
+    as media_kit_video_controls;
 
 final logger = Logger(printer: SimplePrinter(printTime: true));
+
+enum FileType { image, video, unSupport }
+
+final showVideoControl = false;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,15 +38,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String? _mediaPath;
   late final Player _player;
   late final VideoController _controller;
+  late final ImagePicker _picker;
+  Uint8List? _imageBytes;
+
+  FileType? _fileType;
+  Size? _mediaSize;
 
   @override
   void initState() {
     super.initState();
     _player = Player();
     _controller = VideoController(_player);
+    _picker = ImagePicker();
   }
 
   @override
@@ -47,84 +60,197 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
+  //파일 탐색기 파일 선택
   Future<void> _pickFile() async {
-    final result = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowedExtensions: [
-        'jpg',
-        'jpeg',
-        'png',
-        'gif',
-        'bmp',
-        'mp4',
-        'avi',
-        'mov',
-        'mkv',
-        'wmv',
-      ],
-      allowMultiple: false, // 단일 파일만 선택 가능
-    );
+    final XFile? file = await _picker.pickMedia(requestFullMetadata: false);
 
-    if (result != null && result.files.isNotEmpty) {
-      final file = result.files.first;
-      final path = file.path;
-      logger.d('_pickFile: $path');
-      if (path != null) {
-        setState(() {
-          _mediaPath = path;
-        });
-        _player.open(Media(path));
+    if (file != null) {
+      logger.d('Pick File #########');
+      logger.d('file:$file');
+      logger.d('file.name:${file.name}');
+      logger.d('file.mimeType:${file.mimeType}');
+      logger.d('file.path: ${file.path}');
+      final bytes = await file.readAsBytes();
+
+      if (_isImage(file.mimeType, file.path)) {
+        _fileType = FileType.image;
+        _imageBytes = bytes;
+
+        // 이미지 크기 얻기
+        final ui.Image image = await _loadUiImage(bytes);
+        _mediaSize = Size(image.width.toDouble(), image.height.toDouble());
+        logger.d('Media(Video) Size: $_mediaSize');
+      } else if (_isVideo(file.mimeType, file.path)) {
+        _fileType = FileType.video;
+        final playable = await Media.memory(bytes);
+        await _player.setPlaylistMode(PlaylistMode.single);
+        await _player.open(playable);
+        await _controller.waitUntilFirstFrameRendered;
+        if (_player.state.width != null && _player.state.height != null) {
+          _mediaSize = Size(
+            _player.state.width!.toDouble(),
+            _player.state.height!.toDouble(),
+          );
+          logger.d('Media(Video) Size: $_mediaSize');
+        }
+        await _player.play();
+      } else {
+        _fileType = FileType.unSupport;
+        logger.e('지원하지 않는 파일 포맷입니다.');
       }
+      logger.d('_fileType:$_fileType');
+      setState(() {});
+    }
+  }
+
+  //이미지인 경우 이미지 사이즈 구하기 위해
+  Future<ui.Image> _loadUiImage(Uint8List bytes) async {
+    final codec = await ui.instantiateImageCodec(bytes);
+    final frame = await codec.getNextFrame();
+    return frame.image;
+  }
+
+  bool _isImage(String? mimeType, String? path) {
+    final effectiveMimeType = mimeType ?? getMimeTypeFromPath(path ?? '');
+    final ext = effectiveMimeType?.split('/').last.toLowerCase();
+    return ['jpg', 'jpeg', 'png', 'gif', 'bmp'].contains(ext);
+  }
+
+  bool _isVideo(String? mimeType, String? path) {
+    final effectiveMimeType = mimeType ?? getMimeTypeFromPath(path ?? '');
+    final ext = effectiveMimeType?.split('/').last.toLowerCase();
+    return ['mp4', 'avi', 'mov', 'mkv', 'wmv'].contains(ext);
+  }
+
+  //웹에서 mimeType가 null 인경우 path를 통해서 mimeType 결정
+  String? getMimeTypeFromPath(String path) {
+    final ext = path.split('.').last.toLowerCase();
+    switch (ext) {
+      case 'jpg':
+      case 'jpeg':
+        return 'image/jpeg';
+      case 'png':
+        return 'image/png';
+      case 'gif':
+        return 'image/gif';
+      case 'bmp':
+        return 'image/bmp';
+      case 'mp4':
+        return 'video/mp4';
+      case 'avi':
+        return 'video/x-msvideo';
+      case 'mov':
+        return 'video/quicktime';
+      case 'mkv':
+        return 'video/x-matroska';
+      case 'wmv':
+        return 'video/x-ms-wmv';
+      default:
+        return null;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          DropTarget(
-            onDragDone: (details) {
-              if (details.files.isNotEmpty) {
-                final file = details.files.first;
-                final path = file.path;
-                logger.d('onDragDone: $path');
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Stack(
+          children: [
+            //드랍 영역
+            DropTarget(
+              onDragDone: (details) async {
+                if (details.files.isNotEmpty) {
+                  final DropItemFile file = details.files.first as DropItemFile;
+                  logger.d('Drop File #########');
+                  logger.d('file:$file');
+                  logger.d('file.name:${file.name}');
+                  logger.d('file.mimeType:${file.mimeType}');
+                  logger.d('file.path: ${file.path}');
 
-                setState(() {
-                  _mediaPath = path;
-                });
-                _player.open(Media(path));
-              }
-            },
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.cloud_upload, size: 64, color: Colors.blue),
-                    Text('파일을 여기에 드래그하세요.'),
-                  ],
-                ),
-                Video(
-                  controller: _controller,
-                  fit: BoxFit.contain,
-                  fill: Colors.transparent,
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            left: 10,
-            top: 10,
-            child: IconButton(
-              onPressed: () {
-                _pickFile();
+                  final bytes = await file.readAsBytes();
+
+                  if (_isImage(file.mimeType, file.path)) {
+                    _fileType = FileType.image;
+                    _imageBytes = bytes;
+                    // 이미지 크기 얻기
+                    final ui.Image image = await _loadUiImage(bytes);
+                    _mediaSize = Size(
+                      image.width.toDouble(),
+                      image.height.toDouble(),
+                    );
+                    logger.d('Media(Image) Size: $_mediaSize');
+                  } else if (_isVideo(file.mimeType, file.path)) {
+                    _fileType = FileType.video;
+                    final playable = await Media.memory(bytes);
+                    await _player.setPlaylistMode(PlaylistMode.single);
+                    await _player.open(playable);
+                    await _controller.waitUntilFirstFrameRendered;
+
+                    if (_player.state.width != null &&
+                        _player.state.height != null) {
+                      _mediaSize = Size(
+                        _player.state.width!.toDouble(),
+                        _player.state.height!.toDouble(),
+                      );
+                      logger.d('Media(Video) Size: $_mediaSize');
+                    }
+
+                    await _player.play();
+                  } else {
+                    _fileType = FileType.unSupport;
+                    logger.e('지원하지 않는 파일 포맷입니다.');
+                  }
+                  logger.d('_fileType:$_fileType');
+                  setState(() {});
+                }
               },
-              icon: Icon(Icons.folder_open),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.cloud_upload, size: 64, color: Colors.blue),
+                      Text('파일을 여기에 드래그하세요.'),
+                    ],
+                  ),
+                  //이미지, 비디오 영역
+                  _fileType != null
+                      ? _fileType == FileType.video
+                            ? Video(
+                                controller: _controller,
+                                fit: BoxFit.contain,
+                                fill: Colors.transparent,
+                                controls: showVideoControl
+                                    ? media_kit_video_controls
+                                          .AdaptiveVideoControls
+                                    : media_kit_video_controls.NoVideoControls,
+                              )
+                            : _fileType == FileType.image
+                            ? SizedBox.expand(
+                                child: Image.memory(
+                                  _imageBytes!,
+                                  fit: BoxFit.contain,
+                                ),
+                              )
+                            : Center(child: Text('지원하지 않는 파일 포맷입니다.'))
+                      : SizedBox.expand(),
+                ],
+              ),
             ),
-          ),
-        ],
+            Positioned(
+              left: 10,
+              top: 10,
+              child: IconButton(
+                onPressed: () {
+                  _pickFile();
+                },
+                icon: Icon(Icons.folder_open),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
